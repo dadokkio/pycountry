adb910c2564cb0b994db1f8b1969f252a45f4219
Davide Arcuri <davide.arcuri@leonardo.com>
2026-02-18 15:45:47 +0100

    merge diff

--- src/pycountry/db.py
+++ src/pycountry/db.py
@@ -1,109 +1,178 @@
-# vim:fileencoding=utf-8
-
 import json
 import logging
 import threading
-from io import open
+from collections.abc import Callable, Iterator
+from typing import Any, Generic, TypeVar, cast
 
 logger = logging.getLogger("pycountry.db")
 
 
 class Data:
-    def __init__(self, **fields):
+    def __init__(self, **fields: str):
         self._fields = fields
 
-    def __getattr__(self, key):
-        if key not in self._fields:
-            raise AttributeError
-        return self._fields[key]
+    def __getattr__(self, key: str) -> str:
+        if key in self._fields:
+            return self._fields[key]
+        raise AttributeError(key)
 
-    def __setattr__(self, key, value):
+    def __setattr__(self, key: str, value: str) -> None:
         if key != "_fields":
             self._fields[key] = value
-        super(Data, self).__setattr__(key, value)
+        super().__setattr__(key, value)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         cls_name = self.__class__.__name__
         fields = ", ".join("%s=%r" % i for i in sorted(self._fields.items()))
-        return "%s(%s)" % (cls_name, fields)
+        return f"{cls_name}({fields})"
 
-    def __dir__(self):
+    def __dir__(self) -> list[str]:
         return dir(self.__class__) + list(self._fields)
 
+    def __iter__(self):
+        # allow casting into a dict
+        for field in self._fields:
+            yield field, getattr(self, field)
 
-def lazy_load(f):
+
+class Country(Data):
+    pass
+
+
+class Subdivision(Data):
+    pass
+
+
+F = TypeVar("F", bound=Callable[..., Any])
+
+
+def lazy_load(f: F) -> F:
     def load_if_needed(self, *args, **kw):
         if not self._is_loaded:
             with self._load_lock:
                 self._load()
         return f(self, *args, **kw)
 
-    return load_if_needed
+    return cast(F, load_if_needed)
 
 
-class Database:
+T = TypeVar("T", bound=Data)
 
-    data_class_base = Data
-    data_class_name = None
-    root_key = None
-    no_index = []
 
-    def __init__(self, filename):
+class Database(Generic[T]):
+    data_class: type | str
+    root_key: str | None = None
+    no_index: list[str] = []
+    special_index: list[str] = []
+
+    def __init__(self, filename: str) -> None:
         self.filename = filename
         self._is_loaded = False
         self._load_lock = threading.Lock()
 
-    def _load(self):
+        if isinstance(self.data_class, str):
+            self.factory = type(self.data_class, (Data,), {})
+        else:
+            self.factory = self.data_class
+
+    def _clear(self):
+        self._is_loaded = False
+        self.objects = []
+        self.indices = {}
+
+    def _special_index(self, obj, key) -> None:
+        raise NotImplementedError("Must be implemented in subclass")
+
+    def _special_deindex(self, obj, key) -> None:
+        raise NotImplementedError("Must be implemented in subclass")
+
+    def _index_object(self, obj) -> None:
+        for key, value in obj._fields.items():
+            if key in self.no_index:
+                continue
+            if key in self.special_index:
+                self._special_index(obj, key)
+                continue
+            # Lookups and searches are case insensitive. Normalize
+            # here.
+            index = self.indices.setdefault(key, {})
+            value = value.lower()
+            if value in index:
+                logger.debug(
+                    "%s %r already taken in index %r and will be "
+                    "ignored. This is an error in the databases."
+                    % (self.factory.__name__, value, key)
+                )
+            index[value] = obj
+
+    def _deindex_object(self, obj) -> None:
+        for key, value in obj._fields.items():
+            if key in self.no_index:
+                continue
+            if key in self.special_index:
+                self._special_deindex(obj, key)
+                continue
+            value = value.lower()
+            index = self.indices.setdefault(key, {})
+            if value in index:
+                del index[value]
+
+    def _load(self) -> None:
         if self._is_loaded:
             # Help keeping the _load_if_needed code easier
             # to read.
             return
-        self.objects = []
-        self.index_names = set()
-        self.indices = {}
+        self._clear()
 
-        self.data_class = type(
-            self.data_class_name, (self.data_class_base,), {}
-        )
-
-        with open(self.filename, "r", encoding="utf-8") as f:
+        with open(self.filename, encoding="utf-8") as f:
             tree = json.load(f)
 
         for entry in tree[self.root_key]:
-            obj = self.data_class(**entry)
+            obj = self.factory(**entry)
             self.objects.append(obj)
-            # Inject into index.
-            for key, value in entry.items():
-                # Lookups and searches are case insensitive. Normalize
-                # here.
-                value = value.lower()
-                if key in self.no_index:
-                    continue
-                index = self.indices.setdefault(key, {})
-                if value in index:
-                    logger.debug(
-                        "%s %r already taken in index %r and will be "
-                        "ignored. This is an error in the databases."
-                        % (self.data_class_name, value, key)
-                    )
-                index[value] = obj
-
+            # Inject into indices
+            self._index_object(obj)
         self._is_loaded = True
 
     # Public API
 
     @lazy_load
-    def __iter__(self):
+    def add_entry(self, **kw):
+        # create the object with the correct dynamic type
+        obj = self.factory(**kw)
+
+        # append object
+        self.objects.append(obj)
+
+        # update indices
+        self._index_object(obj)
+
+    @lazy_load
+    def remove_entry(self, **kw):
+        # make sure that we receive None if no entry found
+        kw.pop("default", None)
+        obj = self.get(**kw)
+        if not obj:
+            raise KeyError(
+                f"{self.factory.__name__} not found and cannot be removed: {kw}"
+            )
+
+        # remove object
+        self.objects.remove(obj)
+
+        # update indices
+        self._deindex_object(obj)
+
+    @lazy_load
+    def __iter__(self) -> Iterator[T]:
         return iter(self.objects)
 
     @lazy_load
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.objects)
 
     @lazy_load
-    def get(self, **kw):
-        kw.setdefault("default", None)
-        default = kw.pop("default")
+    def get(self, *, default: T | None = None, **kw: str | None) -> T | None:
         if len(kw) != 1:
             raise TypeError("Only one criteria may be given")
         field, value = kw.popitem()
@@ -121,7 +190,7 @@ class Database:
             return default
 
     @lazy_load
-    def lookup(self, value):
+    def lookup(self, value: str) -> T:
         if not isinstance(value, str):
             raise LookupError()
 
@@ -143,7 +212,7 @@ class Database:
                     continue
                 if isinstance(v, list):
                     if value in v:
-                        return candidate
+                        return candidate                
                 if v.lower() == value:
                     return candidate
 
--- src/pycountry/tests/test_general.py
+++ src/pycountry/tests/test_general.py
@@ -1,5 +1,8 @@
 import gettext
+import os.path
 import re
+from importlib import metadata as _importlib_metadata
+from unittest.mock import patch
 
 import pytest
 
@@ -7,19 +10,26 @@ import pycountry
 import pycountry.db
 
 
-@pytest.fixture(autouse=True, scope="session")
-def logging():
-    import logging
+@pytest.fixture
+def countries():
+    pycountry.countries._clear()
+    yield pycountry.countries
+    pycountry.countries._clear()
 
-    logging.basicConfig(level=logging.DEBUG)
+
+@pytest.fixture
+def subdivisions():
+    pycountry.subdivisions._clear()
+    yield pycountry.subdivisions
+    pycountry.subdivisions._clear()
 
 
-def test_country_list():
+def test_country_list(countries):
     assert len(pycountry.countries) == 249
     assert isinstance(list(pycountry.countries)[0], pycountry.db.Data)
 
 
-def test_country_fuzzy_search():
+def test_country_fuzzy_search(countries):
     results = pycountry.countries.search_fuzzy("England")
     assert len(results) == 1
     assert results[0] == pycountry.countries.get(alpha_2="GB")
@@ -59,14 +69,20 @@ def test_country_fuzzy_search():
     assert len(results) == 1
     assert results[0] == pycountry.countries.get(alpha_2="US")
 
+    # bug #231, try initials
+    results = pycountry.countries.search_fuzzy("UK")
+    # There are a lot of matches, but United Kingdom should be one of the top
+    assert len(results) >= 4
+    assert pycountry.countries.get(alpha_2="GB") in results[:4]
 
-def test_historic_country_fuzzy_search():
+
+def test_historic_country_fuzzy_search(countries):
     results = pycountry.historic_countries.search_fuzzy("burma")
     assert len(results) == 1
     assert results[0] == pycountry.historic_countries.get(alpha_4="BUMM")
 
 
-def test_germany_has_all_attributes():
+def test_germany_has_all_attributes(countries):
     germany = pycountry.countries.get(alpha_2="DE")
     assert germany.alpha_2 == "DE"
     assert germany.alpha_3 == "DEU"
@@ -75,8 +91,32 @@ def test_germany_has_all_attributes():
     assert germany.official_name == "Federal Republic of Germany"
 
 
-def test_subdivisions_directly_accessible():
-    assert len(pycountry.subdivisions) == 5127
+def test_missing_common_official(countries):
+    aruba = pycountry.countries.get(alpha_2="AW")
+    assert aruba.alpha_2 == "AW"
+    assert aruba.name == "Aruba"
+    with pytest.raises(AttributeError, match="official_name"):
+        aruba.official_name
+    with pytest.raises(AttributeError, match="common_name"):
+        aruba.common_name
+
+
+def test_missing_common_official_use_different(countries):
+    vietnam = pycountry.countries.get(alpha_2="VN")
+    assert vietnam.alpha_2 == "VN"
+    assert vietnam.name == "Viet Nam"
+    assert vietnam.official_name == "Socialist Republic of Viet Nam"
+    assert vietnam.common_name == "Vietnam"
+
+
+def test_country_missing_attribute(countries):
+    germany = pycountry.countries.get(alpha_2="DE")
+    with pytest.raises(AttributeError):
+        _ = germany.foo
+
+
+def test_subdivisions_directly_accessible(countries):
+    assert len(pycountry.subdivisions) == 5046
     assert isinstance(list(pycountry.subdivisions)[0], pycountry.db.Data)
 
     de_st = pycountry.subdivisions.get(code="DE-ST")
@@ -104,7 +144,7 @@ def test_query_subdivisions_of_country():
 
 
 def test_scripts():
-    assert len(pycountry.scripts) == 182
+    assert len(pycountry.scripts) == 226
     assert isinstance(list(pycountry.scripts)[0], pycountry.db.Data)
 
     latin = pycountry.scripts.get(name="Latin")
@@ -114,7 +154,7 @@ def test_scripts():
 
 
 def test_currencies():
-    assert len(pycountry.currencies) == 170
+    assert len(pycountry.currencies) == 178
     assert isinstance(list(pycountry.currencies)[0], pycountry.db.Data)
 
     argentine_peso = pycountry.currencies.get(alpha_3="ARS")
@@ -124,7 +164,7 @@ def test_currencies():
 
 
 def test_languages():
-    assert len(pycountry.languages) == 7847
+    assert len(pycountry.languages) == 7923
     assert isinstance(list(pycountry.languages)[0], pycountry.db.Data)
 
     aragonese = pycountry.languages.get(alpha_2="an")
@@ -152,10 +192,9 @@ def test_language_families():
 
 def test_locales():
     german = gettext.translation(
-        "iso3166", pycountry.LOCALES_DIR, languages=["de"]
+        "iso3166-1", pycountry.LOCALES_DIR, languages=["de"]
     )
-    german.install()
-    assert _("Germany") == "Deutschland"
+    assert german.gettext("Germany") == "Deutschland"
 
 
 def test_removed_countries():
@@ -167,7 +206,7 @@ def test_removed_countries():
     assert ussr.withdrawal_date == "1992-08-30"
 
 
-def test_repr():
+def test_repr(countries):
     assert re.match(
         "Country\\(alpha_2=u?'DE', "
         "alpha_3=u?'DEU', "
@@ -179,13 +218,26 @@ def test_repr():
     )
 
 
-def test_dir():
+def test_dict(countries):
+    country = pycountry.countries.get(alpha_2="DE")
+    exp = {
+        "alpha_2": "DE",
+        "alpha_3": "DEU",
+        "name": "Germany",
+        "numeric": "276",
+        "official_name": "Federal Republic of Germany",
+        "flag": country.flag,
+    }
+    assert dict(country) == exp
+
+
+def test_dir(countries):
     germany = pycountry.countries.get(alpha_2="DE")
     for n in "alpha_2", "alpha_3", "name", "numeric", "official_name":
         assert n in dir(germany)
 
 
-def test_get():
+def test_get(countries):
     c = pycountry.countries
     with pytest.raises(TypeError):
         c.get(alpha_2="DE", alpha_3="DEU")
@@ -195,7 +247,7 @@ def test_get():
     assert c.get(alpha_2="Foo", default=tracer) is tracer
 
 
-def test_lookup():
+def test_lookup(countries):
     c = pycountry.countries
     g = c.get(alpha_2="DE")
     assert g == c.get(alpha_2="de")
@@ -243,6 +295,235 @@ def test_subdivision_empty_list():
 
 
 def test_has_version_attribute():
+    try:
+        _importlib_metadata.distribution("pycountry")
+    except _importlib_metadata.PackageNotFoundError:
+        pytest.skip("pycountry not installed correctly, you're on your own")
     assert pycountry.__version__ != "n/a"
     assert len(pycountry.__version__) >= 5
     assert "." in pycountry.__version__
+
+
+def test_is_instance_of_language():
+    assert isinstance(pycountry.languages, pycountry.Languages)
+
+
+def test_is_instance_of_country(countries):
+    united_states = pycountry.countries.get(alpha_2="US")
+    class_name = united_states.__class__.__name__
+    assert class_name == "Country"
+
+
+def test_is_instance_of_subdivision(subdivisions):
+    assert isinstance(pycountry.subdivisions, pycountry.Subdivisions)
+
+
+def test_is_instance_of_script():
+    assert isinstance(pycountry.scripts, pycountry.Scripts)
+
+
+def test_is_instance_of_currency():
+    assert isinstance(pycountry.currencies, pycountry.Currencies)
+
+
+def test_country_add_entry(countries):
+    assert pycountry.countries.get(alpha_2="XK") is None
+
+    pycountry.countries.add_entry(
+        alpha_2="XK", alpha_3="XXK", name="Kosovo", numeric="926"
+    )
+
+    country = pycountry.countries.get(alpha_2="XK")
+    assert isinstance(country, pycountry.countries.data_class)
+
+
+def test_country_remove_entry(countries):
+    assert pycountry.countries.get(alpha_2="DE") is not None
+
+    pycountry.countries.remove_entry(alpha_2="DE")
+
+    assert pycountry.countries.get(alpha_2="DE") is None
+
+
+def test_country_remove_non_existent_entry(countries):
+    with pytest.raises(KeyError, match="not found"):
+        pycountry.countries.remove_entry(name="Not A Real Country")
+
+
+def test_subdivision_add_entry(subdivisions):
+    assert subdivisions.get(code="IT-A0") is None
+    it_subdivs = sorted(
+        subdivisions.get(country_code="IT"),
+        key=lambda s: s.code,
+    )
+    assert len(it_subdivs) == 126
+    assert it_subdivs[0] == subdivisions.get(code="IT-21")
+
+    subdivisions.add_entry(
+        code="IT-A0",
+        country_code="IT",
+        name="Valle d'Aosta",
+        parent="23",
+        parent_code="IT-23",
+        type="Province",
+    )
+
+    it_a0 = subdivisions.get(code="IT-A0")
+    assert isinstance(it_a0, subdivisions.data_class)
+
+    # Confirm that indexes were not destroyed
+    it_subdivs = sorted(
+        subdivisions.get(country_code="IT"),
+        key=lambda s: s.code,
+    )
+    assert len(it_subdivs) == 127
+    assert it_subdivs[0] == subdivisions.get(code="IT-21")
+
+
+def test_subdivision_remove_entry(subdivisions):
+    assert subdivisions.get(code="IT-21") is not None
+
+    subdivisions.remove_entry(code="IT-21")
+
+    assert subdivisions.get(code="IT-21") is None
+
+    # Confirm that indexes were not destroyed
+    it_subdivs = sorted(
+        subdivisions.get(country_code="IT"),
+        key=lambda s: s.code,
+    )
+    assert len(it_subdivs) == 125
+    assert it_subdivs[0] == subdivisions.get(code="IT-23")
+
+    # Removing an entry does not remove its children
+    assert subdivisions.match("IT-21")[0].parent_code == "IT-21"
+
+
+def test_no_results_lookup_error(countries):
+    try:
+        import importlib_resources  # type: ignore
+    except ModuleNotFoundError:
+        from importlib import resources as importlib_resources
+
+    def resource_filename(package_or_requirement, resource_name):
+        return str(
+            importlib_resources.files(package_or_requirement) / resource_name
+        )
+
+    DATABASE_DIR = resource_filename("pycountry", "databases")
+    countries = pycountry.ExistingCountries(
+        os.path.join(DATABASE_DIR, "iso3166-1.json")
+    )
+
+    query = "nonexistent query"
+    with pytest.raises(LookupError):
+        countries.search_fuzzy(query)
+
+
+def test_subdivision_fuzzy_search_match(subdivisions):
+    results = pycountry.subdivisions.search_fuzzy("Alabama")
+    assert len(results) == 1
+    assert results[0].name == "Alabama"
+
+
+def test_subdivision_fuzzy_search_partial_match(subdivisions):
+    results = pycountry.subdivisions.search_fuzzy("Massachusett")
+    assert len(results) == 1
+    assert results[0].name == "Massachusetts"
+
+
+def test_subdivision_match(subdivisions):
+    results = pycountry.subdivisions.match("Alabama")
+    assert len(results) == 1
+    assert results[0].name == "Alabama"
+
+
+def test_subdivision_partial_match(subdivisions):
+    results = pycountry.subdivisions.partial_match("Massachusett")
+    assert len(results) == 1
+    assert results[0].name == "Massachusetts"
+
+
+def test_non_country_attribute_error():
+    english = pycountry.languages.get(name="English")
+    with pytest.raises(AttributeError):
+        english.official_name
+
+
+def test_country_attribute_error(countries):
+    canada = pycountry.countries.get(alpha_2="CA")
+    with pytest.raises(AttributeError):
+        canada.maple_syrup
+
+
+def test_with_accents():
+    assert pycountry.remove_accents("Café") == "Cafe"
+    assert pycountry.remove_accents("résumé") == "resume"
+    assert pycountry.remove_accents("naïve") == "naive"
+    assert pycountry.remove_accents("São Paulo") == "Sao Paulo"
+
+
+def test_without_accents():
+    assert pycountry.remove_accents("apple") == "apple"
+    assert pycountry.remove_accents("banana") == "banana"
+
+
+def test_empty_string():
+    assert pycountry.remove_accents("") == ""
+
+
+def test_special_characters():
+    assert pycountry.remove_accents("!@#$%^&*()") == "!@#$%^&*()"
+
+
+def test_unicode_characters():
+    assert pycountry.remove_accents("你好") == "你好"  # Chinese characters
+    assert (
+        pycountry.remove_accents("こんにちは") == "こんにちは"
+    )  # Japanese characters
+
+
+def test_subdivision_search_fuzzy_non_existent_subdivision(subdivisions):
+    with pytest.raises(LookupError):
+        pycountry.subdivisions.search_fuzzy("Non Existent Subdivision")
+
+
+def test_subdivision_partial_match_non(subdivisions):
+    result = pycountry.subdivisions.partial_match("Non Existent Subdivision")
+    assert len(result) == 0
+
+
+def test_subdivision_match_non(subdivisions):
+    result = pycountry.subdivisions.match("Non Existent Subdivision")
+    assert len(result) == 0
+
+
+def test_get_version_with_package_not_found():
+    # Mock importlib.metadata.version to raise PackageNotFoundError
+    with patch(
+        "importlib.metadata.version",
+        side_effect=_importlib_metadata.PackageNotFoundError,
+    ):
+        # Call get_version with a package name that doesn't exist
+        result = pycountry.get_version("non_existent_package")
+
+        # Assert that the result is 'n/a'
+        assert result == "n/a"
+
+
+def test_all_subdivisions_have_name_attribute(subdivisions):
+    has_name_attr = [
+        hasattr(subdivision, "name") for subdivision in subdivisions
+    ]
+    all_have_name_attr = all(has_name_attr)
+
+    assert all_have_name_attr
+
+
+def test_subdivisions_with_missing_parents(subdivisions):
+    result = [
+        (i.code, i.parent_code)
+        for i in pycountry.subdivisions
+        if i.parent_code and not i.parent
+    ]
+    assert result == []
